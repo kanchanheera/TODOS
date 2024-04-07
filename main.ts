f5bb96c54afb61e797eba393e8a484267309715a
Kanchan Heera <161306339+kanchanheera@users.noreply.github.com>
2024-04-07 22:19:25 +0500

    make it advance by adding extra options.

--- main.ts
+++ main.ts
@@ -3,31 +3,79 @@
 import chalk from "chalk";
 import inquirer from "inquirer"
 
-let todos = [];
+let todos: string [] = [];
 let condition = true;
+console.log(chalk.magentaBright(`\n\t WELCOME TO TODO LIST \n\t`));
+
 
 while(condition)
 {
-    let addTask = await inquirer.prompt(
-    [
+    let answer = await inquirer.prompt(
+        [
         {
-            name: 'todo',
-            type: 'input',
-            message: chalk.cyanBright ('What you want to add in your todos?')
+            name: 'select',
+            type: 'list',
+            message: chalk.yellow ('Select an option:'),
+            choices: ['Add', 'Update', 'View todo list', 'Delete', 'Exit'],
         },
- 
-        {
-            name: 'addMore',
-            type: 'confirm',
-            message: chalk.cyanBright('Do you want to add more?'),
-            default:'true'
-        }
     ]
 );
-
-todos.push(addTask.todo);
-condition = addTask.addMore;
-console.log(todos);
-
+if(answer.select === 'Add'){
+    let addTodo = await inquirer.prompt({
+        name: 'todo',
+        type: 'input',
+        message: chalk.green('Add items in your list:'),
+        validate: function (input){
+            if(input.trim() == ""){
+                return chalk.redBright.bold(`Invalid input! Task not added.`);
+            }
+            return true;
+        }
+    });
+    if(addTodo.todo.trim() !== ""){
+    todos.push(addTodo.todo);
+    todos.forEach(todo => console.log(todo)
+     )
+    }
+}
+if(answer.select === 'Update'){
+    let updateTodo = await inquirer.prompt({
+        name: 'todo',
+        type: 'list',
+        message: chalk.green('Select an item which do you want to update:'),
+        choices: todos.map(item => item)
+    })
+    let addTodo = await inquirer.prompt({
+        name: 'todo',
+        type: 'input',
+        message: chalk.green('Add items in your list:'),
+    });
+    let newTodo = todos.filter(val =>val !== updateTodo.todo);
+    todos = [...newTodo,addTodo.todo];
+    todos.forEach(todo => console.log(todo)
+    )
+}
+if(answer.select === 'View todo list'){
+    console.log(chalk.magenta.bold(`\n\t *** TO-DO LIST *** \t`));
+    todos.forEach(todo => console.log(todo)
+    )
+}
+ if(answer.select === 'Delete'){
+    let deleteTodo = await inquirer.prompt({
+        name: 'todo',
+        type: 'list',
+        message: chalk.green('Select an item to delete:'),
+        choices: todos.map(item => item)
+    });
+    let newTodo = todos.filter(val =>val !== deleteTodo.todo);
+    todos = [...newTodo];
+    todos.forEach(todo => console.log(todo)
+    )
+}
+if(answer.select === 'Exit'){
+    console.log(chalk.red.bold(`\t Exiting Program...\t`));
+    condition = false;
+    
+}
 }
 
